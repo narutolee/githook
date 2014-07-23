53798a4c97fcd296d852dae1b2bd4a06aeeee179
narutolee <t080224@126.com>
2014-07-23 11:39:02 +0800

    Update update.php

--- update.php
+++ update.php
@@ -35,14 +35,14 @@ if($content){
 	}
 }*/
 
-error_reporting ( E_ALL );
-$dir = '/data/test/';//该目录为git检出目录
-$handle = popen('cd '.$dir.' && git pull 2>&1','r');
-$read = stream_get_contents($handle);
-printf($read);
-pclose($handle);
+//error_reporting ( E_ALL );
+//$dir = '/data/test/';//该目录为git检出目录
+//$handle = popen('cd '.$dir.' && git pull 2>&1','r');
+//$read = stream_get_contents($handle);
+//printf($read);
+//pclose($handle);
 
-
+printf("abc");
 
 
 ?>
